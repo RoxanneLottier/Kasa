46dac82cc1a8ca586994cbb0a77060cd9dbfa183
RoxanneLottier <roxanneleger@msn.com>
2023-07-26 12:44:52 +0200

    start rating + collapse height adapted

--- src/pages/A-Propos/index.js
+++ src/pages/A-Propos/index.js
@@ -12,22 +12,22 @@ function APropos() {
             <CoverHeader src={coverImage} name="the-mountains-when-it-snows" title=""/>
             <div className={styles.content}>
                 <Collapse title="Fiabilité">
-                    <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont
+                    <p className={styles.paragraph}>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont
                                     régulièrement vérifiées par nos équipes.</p>
                 </Collapse>
                 <br/>
                 <Collapse title="Respect">
-                    <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
+                    <p className={styles.paragraph}>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
                           perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
                 </Collapse>
                 <br/>
                 <Collapse title="Service">
-                    <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
+                    <p className={styles.paragraph}>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
                           perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
                 </Collapse>
                 <br/>
                 <Collapse title="Sécurité">
-                    <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement
+                    <p className={styles.paragraph}>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement
                           correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au
                           locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons
                           également des ateliers sur la sécurité domestique pour nos hôtes.</p>
